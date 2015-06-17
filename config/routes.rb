24c8dda3cb153db6a266f4fbc3e4c7f05d1f7d14
Mil Reyes <milci.10@gmail.com>
2015-06-17 16:51:43 -0600

    Added basic user registration functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
+  get "log_out" => "sessions#destroy", as: "log_out"
+  get "log_in" => "sessions#new", as: "log_in"
+  get "sign_up" => "users#new", as: "sign_up"
+  root :to => "users#new"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   resources :specs
   # You can have the root of your site routed with "root"
-  root 'specs#index'
+  resources :users
+  resources :sessions
+
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
